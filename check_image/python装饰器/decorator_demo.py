5a69cbc69a1b3393c19a7f519dca268b61d2d1ed
cathy77 <stepxuxu@gmail.com>
2019-07-23 14:16:36 +0800

    no message

--- check_image/python装饰器/decorator_demo.py
+++ check_image/python装饰器/decorator_demo.py
@@ -1,1 +1,28 @@
 # -*- coding:utf-8 -*-
+
+#给函数加上打印结果
+def print_result(f):
+    def new_f(*args,**kw):
+        result = f(*args,**kw)
+        print('函数名是:%s,和是:%s' %(f.__name__,result))
+    return new_f
+
+
+@print_result
+def add(a,b):
+    return a+b
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import math
+
+    r = isinstance(int(math.sqrt(4)),int)
+
+    print(r)
