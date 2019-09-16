53de52aec9a780b9c8ba1f35623013230e1476f8
77dx <396321556@qq.com>
2019-09-16 10:08:33 +0800

    人保-模型验证

--- base64/ocrTest/baiduOCR.py
+++ base64/ocrTest/baiduOCR.py
@@ -13,6 +13,7 @@ import datetime
 接口请求
 '''
 log = Loggers(level='info')
+token = 'cd77f6c43eb04767af5bb65ca19f7d48'
 
 #获取access_token
 def access_token():
@@ -33,6 +34,14 @@ def ocrApi(token,filename):
     response = json.loads(d)
     return response["words_result"]
 
+#获取七牛token
+def get_qiniuToken():
+    header = {"Accept":"application/json, text/javascript, */*; q=0.01","Content-Type":"application/json","token":token}
+    r = requests.post("http://192.168.5.111/customer/qiniu/getQiNiuToken",headers=header)
+    d = r.text
+    s = json.loads(d)
+    return s["data"]["qiNiuToken"]
+
 #图片转base64
 def open_image(filename):
     f = open(filename,'rb')
@@ -41,14 +50,39 @@ def open_image(filename):
     return s
 
 #上传图片数据流
-def upload(token,base):
+def upload(qiniu_token,base):
     data = base
-    header = {"Authorization":"UpToken "+token,"Content-Type":"application/octet-stream"}
+    header = {"Authorization":"UpToken "+qiniu_token,"Content-Type":"application/octet-stream","token":token}
     r = requests.post('https://upload-z2.qiniup.com/putb64/-1',data=data,headers=header)
     d = r.text
+    print(d)
     s = json.loads(d)
     key = s["key"]
     return key
+
+#提交腾讯OCR
+def submit_tencent(key):
+    url = "http://192.168.5.111/customer/demo/tencentOCR?url="+key
+    r = requests.get(url)
+    d = r.text
+    s = json.loads(d)
+    return s["data"]
+
+#提交阿里OCR
+def submit_alibaba(key):
+    url = "http://192.168.5.111/customer/demo/aliOCR?url="+key
+    r = requests.get(url)
+    d = r.text
+    s = json.loads(d)
+    return s["data"]
+
+#提交百度OCR
+def submit_baidu(key):
+    url = "http://192.168.5.111/customer/demo/baiduOCR?url="+key
+    r = requests.get(url)
+    d = r.text
+    s = json.loads(d)
+    return s["data"]
 
 '''
 结果保存到excel
@@ -63,7 +97,7 @@ def xls_imageUrl(url,filename):
     newsheet.write(rownum, 0, url)
     newbook.save(filename)
 
-#把content保存excel
+#把content1保存excel
 def xls_content(content,xls_name):
     workbook = xlrd.open_workbook(xls_name, formatting_info=True)
     sheet = workbook.sheets()[0]
@@ -72,6 +106,28 @@ def xls_content(content,xls_name):
     rownum = sheet.nrows
     newsheet.write(rownum-1, 1, content)
     newbook.save(xls_name)
+
+#把content2保存excel
+def xls_content_ali(content,xls_name):
+    workbook = xlrd.open_workbook(xls_name, formatting_info=True)
+    sheet = workbook.sheets()[0]
+    newbook = copy(workbook)
+    newsheet = newbook.get_sheet(0)
+    rownum = sheet.nrows
+    newsheet.write(rownum-1, 2, content)
+    newbook.save(xls_name)
+
+
+#把content3保存excel
+def xls_content_baidu(content,xls_name):
+    workbook = xlrd.open_workbook(xls_name, formatting_info=True)
+    sheet = workbook.sheets()[0]
+    newbook = copy(workbook)
+    newsheet = newbook.get_sheet(0)
+    rownum = sheet.nrows
+    newsheet.write(rownum-1, 3, content)
+    newbook.save(xls_name)
+
 
 #新建excel文件
 def new_xls(xls_name):
@@ -82,13 +138,37 @@ def new_xls(xls_name):
         log.logger.warning(xls_name+"不存在")
     book = xlwt.Workbook()
     sheet = book.add_sheet("sheet1")
-    title = ["image","content",]
+    title = ["image","tencent","alibaba","baidu"]
     for col in range(len(title)):
         sheet.write(0, col, title[col])
     book.save(xls_name)
 
 
-#ocr测试
+#OCR测试
+def tencentTest(image_url,xls_name):
+    # 创建xls文件
+    new_xls(xls_name)
+    # 遍历图片文件
+    image_url = image_url
+    imageList = os.listdir(image_url)
+    for i in imageList:
+        image = image_url + '/' + i
+        xls_imageUrl(image,xls_name)
+        print(image)
+        qiniu_token = get_qiniuToken()
+        print(qiniu_token)
+        base = open_image(image)
+        key = upload(qiniu_token,base)
+        print(key)
+        content = submit_tencent(key)#腾讯结果
+        print(content)
+        xls_content(content,xls_name)
+        content_ali = submit_alibaba(key)  #阿里结果
+        xls_content_ali(content_ali,xls_name)
+        content_baidu = submit_baidu(key)   #百度结果
+        xls_content_baidu(content_baidu,xls_name)
+
+#百度ocr测试
 def ocrTest(image_url,xls_name):
     #创建xls文件
     new_xls(xls_name)
@@ -115,7 +195,7 @@ if __name__ == '__main__':
     #开始时间
     start = datetime.datetime.now()
     #执行测评
-    ocrTest('D:/ThyroidBarImage', "baidu_2.xls")
+    tencentTest('D:/test', "result.xls")
     #结束时间
     end = datetime.datetime.now()
     print('运行时长：'+str((end - start).seconds)+'秒')
--- base64/renbao/image_base64.py
+++ base64/renbao/image_base64.py
@@ -8,20 +8,23 @@ import xlwt
 import xlrd
 from xlutils.copy import copy
 from renbao.logger import Loggers
+import pandas as pd
 import datetime
 
 '''
 接口请求
 '''
-base_url = "http://192.168.5.111"
+base_url = "http://api.flins.com.cn"
+# base_url = "http://mp.flins.com.cn"
 log = Loggers(level='info')
 
 #获取七牛token
 def get_qiniuToken():
     header = {"Accept":"application/json, text/javascript, */*; q=0.01","Content-Type":"application/json"}
-    r = requests.post(base_url+'/business/rb/getQiNiuToken',headers=header)
+    r = requests.post(base_url+"/business/rb/getQiNiuToken",headers=header)
     d = r.text
     s = json.loads(d)
+    log.logger.info(s)
     return s["data"]["qiNiuToken"]
 
 #图片转base64
@@ -32,86 +35,99 @@ def open_image(filename):
     return s
 
 #上传图片数据流
-def upload(token,base):
+def upload(qiniu_token,base):
     data = base
-    header = {"Authorization":"UpToken "+token,"Content-Type":"application/octet-stream"}
+    header = {"Authorization":"UpToken "+qiniu_token,"Content-Type":"application/octet-stream"}
     r = requests.post('https://upload-z2.qiniup.com/putb64/-1',data=data,headers=header)
     d = r.text
     s = json.loads(d)
     key = s["key"]
-    xls_key(key)
+    log.logger.info(s)
     return key
 
 #提交图片
-def submit(key):
-    data = {"imgUrl":key}
-    header = {"Accept":"application/json, text/javascript, */*; q=0.01", "Content-Type": "application/json","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"}
-    r = requests.post(base_url+"/business/rb/submit",data=json.dumps(data),headers=header)
+def submit(key,file):
+    data = {"imgUrl": key}
+    header = {"Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/json",
+              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"}
+    r = requests.post(base_url + "/business/rb/submit", data=json.dumps(data), headers=header)
     d = r.text
+    s = json.loads(d)
     try:
-        if d:
-            s = json.loads(d)
-            return s["data"]["id"]
+        log.logger.info(s)
+        return s["data"]["id"]
     except:
-        log.logger.error(key+"提交失败")
+        data_list = [s["code"],s["desc"]]
+        xls(data_list,file)
+        log.logger.error(s + "提交失败")
 
 #获取测评结果
-def query(id):
+def query(id,file):
     data = {"id":id}
     header = {"Accept": "application/json, text/javascript, */*; q=0.01", "Content-Type": "application/json",
               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"}
     r = requests.post(base_url+'/business/rb/query',data=json.dumps(data),headers=header)
     d = r.text
     s = json.loads(d)
-    data_list = [s["data"]["id"],s["data"]["imgUrl"],s["data"]["content"],s["data"]["tiRands"],s["data"]["acr"],s["data"]["pointEcho"]]
-    log.logger.info(data_list)
-    xls(data_list)
+    data_list = [s["data"]["id"],s["data"]["imgUrl"],s["data"]["content"],s["data"]["tiRands"],s["data"]["acr"],s["data"]["pointEcho"],s["data"]["otherEcho"]]
+    log.logger.info(s)
+    xls(data_list,file)
+
 
 '''
 结果保存到excel
 '''
-#把七牛key保存excel
-def xls_key(key):
+#把code保存excel
+def xls_code(key):
     workbook = xlrd.open_workbook('data.xls', formatting_info=True)
     sheet = workbook.sheets()[0]
     newbook = copy(workbook)
     newsheet = newbook.get_sheet(0)
     rownum = sheet.nrows
-    newsheet.write(rownum, 0, key)
+    newsheet.write(rownum-1, 1, key)
     newbook.save('data.xls')
 
+#把image保存excel
+def xls_image(file,image):
+    workbook = xlrd.open_workbook(file, formatting_info=True)
+    sheet = workbook.sheets()[0]
+    newbook = copy(workbook)
+    newsheet = newbook.get_sheet(0)
+    rownum = sheet.nrows
+    newsheet.write(rownum, 0, image)
+    newbook.save(file)
+
 #把测评结果保存excel
-def xls(data_list):
-    workbook = xlrd.open_workbook('data.xls', formatting_info=True)
+def xls(data_list,file):
+    workbook = xlrd.open_workbook(file, formatting_info=True)
     sheet = workbook.sheets()[0]
     newbook = copy(workbook)
     newsheet = newbook.get_sheet(0)
     rownum = sheet.nrows
     for col in range(len(data_list)):
         newsheet.write(rownum-1, col+1, data_list[col])
-    newbook.save('data.xls')
+    newbook.save(file)
 
 #新建excel文件
-def new_xls():
+def new_xls(file):
     try:
-        if os.path.exists('data.xls'):
-            os.remove('data.xls')
+        if os.path.exists(file):
+            os.remove(file)
     except :
         log.logger.warning("data.xls不存在")
     book = xlwt.Workbook()
     sheet = book.add_sheet("sheet1")
-    title = ["key","id", u"imgUrl",u"content", u"tiRands", u"acr", u"pointEcho"]
+    title = ["image","id",u"imgUrl",u"content", u"tiRands", u"acr", u"pointEcho","otherEcho"]
     for col in range(len(title)):
         sheet.write(0, col, title[col])
     book.save('data.xls')
 
 
 #人保B超测评
-def renbao():
+def renbao(image_url,file):
     #创建xls文件
-    new_xls()
+    new_xls(file)
     # 遍历图片文件
-    image_url = 'D:/image'
     imageList = os.listdir(image_url)
     for i in imageList:
         image = image_url + '/' + i
@@ -119,20 +135,49 @@ def renbao():
             token = get_qiniuToken()  # 获取七牛token
             base = open_image(image)  # 图片转base64
             key = upload(token, base)  # 上传图片
-            id = submit(key)  # 提交测评#
-            query(id)  # 获取测评结果
+            id = submit(key,file)  # 提交测评#
+            query(id,file)  # 获取测评结果
         except:
             continue
 
-if __name__ == '__main__':
+'''
+    人保B超测评-samples图片验证2019-9-16
+    先把label.xls image列的图片名称取出来
+    存进列表，再循环取出拼接图片本地地址，
+    再请求接口并保存接口返回内容。
+'''
+def renbao_sample(image_url,file):
+    new_xls(file)
+    df = pd.read_excel('label.xls')
+    image = df['image'].values
+    list = image.tolist()
+    for i in list:
+        try:
+            image = image_url + "/" + i
+            xls_image(file,i)
+            log.logger.info("image:"+i)
+            qiniutoken = get_qiniuToken()  # 获取七牛token
+            base = open_image(image)  # 图片转base64
+            key = upload(qiniutoken, base)  # 上传图片
+            id = submit(key,file)  # 提交测评#
+            query(id,file)  # 获取测评结果
+        except:
+            continue
 
+
+
+
+if __name__ == '__main__':
     #开始时间
     start = datetime.datetime.now()
     #执行人保测评
-    renbao()
-    #结束时间
+    renbao_sample('D:/samples','data.xls')
+   # 结束时间
     end = datetime.datetime.now()
     print('运行时长：'+str((end - start).seconds)+'秒')
+
+
+
 
 
 
